1673f01786ffdb19993f1ca5b1a8e2400345f36d
Asura0107 <mircoorosio@gmail.com>
2023-12-06 09:00:43 +0100

    U3-W3-D2

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.scss']
+  styleUrls: ['./reactive.component.scss'],
 })
 export class ReactiveComponent implements OnInit {
-
-  constructor() { }
-
+  form!: FormGroup;
+  generi = ['uomo', 'donna'];
+  constructor(private fb: FormBuilder) {}
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  // });
   ngOnInit(): void {
+    this.form = this.fb.group({
+      userInfo: this.fb.group({
+        Nome: this.fb.control(null, {
+          validators: [Validators.required],
+        }),
+        Cognome: this.fb.control(null, [Validators.required]),
+        username: this.fb.control(''),
+      }),
+      secret: this.fb.group({
+        password: this.fb.control(''),
+        rifai: this.fb.control(null, {
+          validators: [Validators.required],
+        }),
+      }),
+      genere: this.fb.control('donna'),
+      profilo: this.fb.control(''),
+      biografia: this.fb.control(''),
+    });
   }
 
+  Submit() {
+    // console.log(this.profileForm);
+    console.log(this.form);
+  }
 }
